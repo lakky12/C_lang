ee0fb20c8ba4146c1bf4c12ca51c86898a8ee88c
unknown <derangulasaipranathi2007@gmail.com>
2025-06-28 15:40:49 +0530

    Operators

--- Task_1.c
+++ Task_1.c
@@ -4,39 +4,15 @@ int main()
 	int a;
 	int b;
 	int c;
-	int d;
-	int e;
-	int f;
-	int g;
-	int h;
-	int  i;
-	int j;
 
-	printf(" Enter the value of a:");
+	printf("enter the value of a:");
 	scanf("%d",&a);
 
-	printf("the value of b:");
+	printf("enter the value of b:");
 	scanf("%d",&b);
 
-	c=a+b;
-	printf("The value of c:%d\n",c);
+	c= a<b;
 
-	d=a-b;
-	printf("The value of d:%d\n",d);
-
-	e=a*b;
-	printf("The value of e:%d\n",e);
-
-	f=a/b;
-	printf("The  value of f:%d\n",f);
-
-	g=a%b; 
-	printf("The value of g:%d\n",g);
-
-	i=a>b;
-	printf("The value of i:%d\n",i);
-
-	j=a**b;
-	printf("The value of j:%d\n",j);
-	return 1;
+	printf("enter the value of c:%d",c);
+	return 0;
 }
