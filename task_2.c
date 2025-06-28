f15272b52963abffc9ce604bdf57ccd616851ab3
unknown <derangulasaipranathi2007@gmail.com>
2025-06-28 15:36:35 +0530

    String tasks

---
+++ task_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	char a;
+	char b;
+
+	printf("enter the value of a:");
+	scanf("%c\n",&a);
+	printf("the value of a:%c\n",a);
+
+	printf("enter the value of b:");
+	scanf("%c\n",&b);
+	printf("the value of b:%c\n",b);
+}
