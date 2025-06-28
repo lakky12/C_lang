3a8af4b6465d039466209b04f678c6fb775fb2b9
unknown <derangulasaipranathi2007@gmail.com>
2025-06-28 15:46:03 +0530

    task 3

---
+++ Char-task_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+char main()
+{
+	char fruit;
+	char school_name;
+	char pen_name;	
+	printf("enter the name of fruit:\n",fruit);
+	scanf("%c",&fruit);
+	print("enter the school_name:\n",school_name);
+	scanf("%c",&school_name);
+	printf("enter the pen_name:\n",pen_name);
+	scanf("%c",&pen_name);
+}
